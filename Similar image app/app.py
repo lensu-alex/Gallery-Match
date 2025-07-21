bb495dda2160b4234506036dcfd34e57a183d5cd
lensu-alex <lensu.alex@gmail.com>
2025-07-22 02:32:18 +0300

    Update app.py

--- Similar image app/app.py
+++ Similar image app/app.py
@@ -8,13 +8,11 @@ import io
 
 app = Flask(__name__)
 
-# Folders
 QUERY_FOLDER = 'static/uploaded_query'
 GALLERY_FOLDER = 'static/uploaded_gallery'
 os.makedirs(QUERY_FOLDER, exist_ok=True)
 os.makedirs(GALLERY_FOLDER, exist_ok=True)
 
-# Clean folders on start
 for folder in [QUERY_FOLDER, GALLERY_FOLDER]:
     for f in os.listdir(folder):
         os.remove(os.path.join(folder, f))
@@ -29,7 +27,6 @@ def index():
         for f in os.listdir(GALLERY_FOLDER):
             os.remove(os.path.join(GALLERY_FOLDER, f))
 
-        # Save query image
         uploaded_query = request.files.get('query_image')
         if uploaded_query and uploaded_query.filename != '':
             filename = secure_filename(uploaded_query.filename)
@@ -37,14 +34,12 @@ def index():
             uploaded_query.save(query_path)
             query_image = f"uploaded_query/{filename}"
 
-        # Save gallery images
         gallery_files = request.files.getlist('gallery_images')
         for file in gallery_files:
             if file and file.filename != '':
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(GALLERY_FOLDER, filename))
 
-        # Run similarity search
         if query_image:
             full_query_path = os.path.join("static", query_image)
             results = find_similar_images(full_query_path, GALLERY_FOLDER)
@@ -53,10 +48,8 @@ def index():
 
 @app.route('/download_matches')
 def download_matches():
-    # Collect the top-matched images from the last result
     matched_images = request.args.getlist("files")
 
-    # Create in-memory ZIP
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
         for file_rel_path in matched_images:
